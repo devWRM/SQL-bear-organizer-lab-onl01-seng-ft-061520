49d0fc70c2a23715e015e0f5d936ef0acc89dc20
devWRM <mcwen@comcast.net>
2020-07-13 17:17:52 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "milk chocolate", "calm", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 29, "F", "grey", "tense", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 40, "F", "auburn", "energetic", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 35, "F", "dark brown", "alert", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 18, "F", "black", "thoughtful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 52, "M", "jade", "bitter", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 202, "F", "rainbow", "bright", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 29, "M", "tan", "brute", 0);
+
+
