f47ebd52649c1d4c189742fe551eeefc4b8e7476
devWRM <mcwen@comcast.net>
2020-07-14 06:13:08 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,18 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
+
+  # VIDEO explains why GROUP BY used:
+  #     https://www.youtube.com/watch?v=IudbCqjCGAc
  
-  "SELECT bears.color, COUNT(bears.color) FROM bears GROUP BY bears.color ORDER BY COUNT(*) DESC LIMIT 1;"
+  # "SELECT bears.color, COUNT(bears.color) FROM bears GROUP BY bears.color ORDER BY COUNT(*) DESC LIMIT 1;"
    
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
+
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(color) FROM bears WHERE temperament = 'goofy';"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
